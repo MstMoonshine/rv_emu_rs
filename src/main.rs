a0cb358f57cf927e0e2b78586ef335edcd35b683
MstMoonshine <mingde-ren@outlook.com>
2022-01-22 00:22:37 +0800

    doc: binary asm discription

--- src/main.rs
+++ src/main.rs
@@ -14,35 +14,19 @@ mod rv_system;
 // 0x0032a023, // sw x3, 0(x5)
 
 fn main() {
-
-// li	x1, 1
-// li	x2, 2
-// add	x3, x1, x2
-
-// li	x4, 0x80000000
-// sw	x3, 0(x4)
-
-// li	x5, 0xdeadbeef
-// lw	x6, 0(x4)
-// add	x7, x5, x6
-
-// sw	x7, 4(x4)
-// sh	x7, 8(x4)
-// sb	x7, 12(x4)
-
     let file = &[
-        0x00100093,
-        0x00200113,
-        0x002081b3,
-        0x80000237,
-        0x00322023,
-        0xdeadc2b7,
-        0xeef28293,
-        0x00022303,
-        0x006283b3,
-        0x00722223,
-        0x00721423,
-        0x00720623,
+        0x00100093, // li	x1, 1
+        0x00200113, // li	x2, 2
+        0x002081b3, // add	x3, x1, x2
+        0x80000237, // li	x4, 0x80000000
+        0x00322023, // sw	x3, 0(x4)
+        0xdeadc2b7, // li	x5, 0xdeadbeef
+        0xeef28293, // (cont.)
+        0x00022303, // lw	x6, 0(x4)
+        0x006283b3, // add	x7, x5, x6
+        0x00722223, // sw	x7, 4(x4)
+        0x00721423, // sh	x7, 8(x4)
+        0x00720623, // sb	x7, 12(x4)
     ];
 
     let rv32_sys = RV32System::new(file);
