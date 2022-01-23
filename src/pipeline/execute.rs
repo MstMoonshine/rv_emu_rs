fabf0243c5e0de77c5a84e652b6fe7609dc7e80f
MstMoonshine <mingde-ren@outlook.com>
2022-01-23 19:50:19 +0800

    feat: support all ALU instruction

--- src/pipeline/execute.rs
+++ src/pipeline/execute.rs
@@ -7,11 +7,12 @@ use super::{decode::Decode, PipelineStage, Stage};
 #[derive(Debug, Clone, Copy, TryFromPrimitive)]
 #[repr(u32)]
 enum ALUOperation {
-    ADD = 0b000,
+    ADD = 0b000, // also SUB
     SLL = 0b001,
     SLT = 0b010,
+    SLTU = 0b011,
     XOR = 0b100,
-    SRL = 0b101,
+    SRL = 0b101, // also SRA
     OR  = 0b110,
     AND = 0b111,
 }
@@ -20,9 +21,11 @@ enum ALUOperation {
 #[derive(Debug, Clone, Copy)]
 pub struct ExecutionValues {
     pub rd:             u32,
-    pub funct3:          u32,
+    pub funct3:         u32,
     pub rs1:            u32,
     pub rs2:            u32,
+    pub imm11_0:        u32,
+    pub shamt:          u32,
     pub imm32:          i32,
 
     pub alu_result:     u32,
@@ -39,6 +42,8 @@ impl ExecutionValues {
             funct3:             0,
             rs1:                0,
             rs2:                0,
+            imm11_0:            0,
+            shamt:              0,
             imm32:              0,
 
             alu_result:         0,
@@ -85,6 +90,8 @@ impl PipelineStage for Execute {
         exe_val.funct3 = de_val.funct3;
         exe_val.rs1 = de_val.rs1;
         exe_val.rs2 = de_val.rs2;
+        exe_val.imm11_0 = de_val.imm11_0;
+        exe_val.shamt = de_val.shamt;
         exe_val.imm32 = de_val.imm32;
         exe_val.is_alu_operation = de_val.is_alu_operation;
         exe_val.is_store = de_val.is_store;
@@ -93,6 +100,8 @@ impl PipelineStage for Execute {
 
         let is_register_op = (de_val.opcode >> 5) & 1 == 1;
         let is_alternate = (de_val.imm11_0 >> 10) & 1 == 1;
+
+        let right_number = if is_register_op { de_val.rs2 } else { de_val.imm11_0 };
 
         exe_val.alu_result = match ALUOperation::try_from(de_val.funct3) {
             Ok(ALUOperation::ADD) => {
@@ -107,14 +116,32 @@ impl PipelineStage for Execute {
                 }
             },
 
-            Ok(ALUOperation::SLL) => de_val.rs1 << (de_val.rs2 & 0x1F),
-            Ok(ALUOperation::SRL) => de_val.rs1 >> (de_val.rs2 & 0x1F),
+            Ok(ALUOperation::SLL) => if is_register_op {
+                de_val.rs1 << (de_val.rs2 & 0x1f)
+            } else {
+                de_val.rs1 << (de_val.shamt)
+            },
 
-            Ok(ALUOperation::SLT) => if de_val.rs1 < de_val.rs2 { 1_u32 } else { 0_u32 },
+            Ok(ALUOperation::SRL) => {
+                let src = de_val.rs1;
+                let shamt = if is_register_op {
+                    de_val.rs2
+                } else {
+                    de_val.shamt
+                };
 
-            Ok(ALUOperation::AND) => de_val.rs1 & de_val.rs2,
-            Ok(ALUOperation::OR) => de_val.rs1 | de_val.rs2,
-            Ok(ALUOperation::XOR) => de_val.rs1 ^ de_val.rs2,
+                if is_alternate {
+                    ((de_val.rs1 as i32) >> (shamt & 0x1f)) as u32 // SRA
+                } else {
+                    de_val.rs1 >> (shamt & 0x1F)                   // SRL
+                }
+            },
+            Ok(ALUOperation::SLTU) => if de_val.rs1 < right_number { 1_u32 } else { 0_u32 },
+            Ok(ALUOperation::SLT) => if (de_val.rs1 as i32) < (right_number as i32) { 1_u32 } else { 0_u32 },
+
+            Ok(ALUOperation::AND) => de_val.rs1 & right_number,
+            Ok(ALUOperation::OR) => de_val.rs1 | right_number,
+            Ok(ALUOperation::XOR) => de_val.rs1 ^ right_number,
 
             _ => {
                 // println!("Unimplemented! funct3 = {:#05b}, instruction = {:#010x}",
